4a54bc7f8b4ccdd8814f586a6d9c958fb7eded34
André Reis Brandt <33246919+AndreRBrandt@users.noreply.github.com>
2023-06-23 22:39:48 -0300

    update: Contract.php para o segundo passo do teste

--- classes/Contract.php
+++ classes/Contract.php
@@ -11,11 +11,15 @@ class Contract
 
     public function getContracts()
     {
-        $sql = "SELECT Tb_banco.nome AS nome_banco, Tb_convenio.verba, Tb_contrato.codigo AS codigo_contrato, Tb_contrato.data_inclusao, Tb_contrato.valor, Tb_contrato.prazo
+        $sql = "SELECT Tb_banco.nome AS nome_banco, Tb_convenio.verba,
+                MIN(Tb_contrato.data_inclusao) AS data_inclusao_antiga,
+                MAX(Tb_contrato.data_inclusao) AS data_inclusao_nova,
+                SUM(Tb_contrato.valor) AS soma_valor
                 FROM Tb_contrato
                 INNER JOIN Tb_convenio_servico ON Tb_contrato.convenio_servico = Tb_convenio_servico.codigo
                 INNER JOIN Tb_convenio ON Tb_convenio_servico.convenio = Tb_convenio.codigo
-                INNER JOIN Tb_banco ON Tb_convenio.banco = Tb_banco.codigo";
+                INNER JOIN Tb_banco ON Tb_convenio.banco = Tb_banco.codigo
+                GROUP BY Tb_banco.nome, Tb_convenio.verba";
 
         return $this->db->executeQuery($sql);
     }
